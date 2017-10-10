152310354f2c322594a2303e569bd2af3d3756b4
Joe <rezenbiz@gmail.com>
2017-10-10 00:04:18 -0700

    Added basic components I'll need.

---
+++ jammming/src/Components/Track/Track.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Track.css';
+
+class Track extends Component {
+  render() {
+
+    renderAction() {
+       if (this.props.isRemoval) {
+         return <a className="Track-action" onClick={this.removeTrack}>-</a>
+       }
+       return <a className="Track-action" onClick={this.addTrack}>+</a>;
+     }
+
+    return (
+      <div className="Track">
+  <div className="Track-information">
+    <h3><!-- track name will go here --></h3>
+    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+  </div>
+  <a className="Track-action"><!-- + or - will go here --></a>
+</div>
+    );
+  }
+}
+
+export default Track;
